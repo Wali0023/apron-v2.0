d65c6e366940793f2f77064d053f9e8e961ee5ed
Ulfath <ulfathwaliullah@gmail.com>
2026-03-16 01:42:29 +0100

    Add header

---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header className="bg-[#F5E7C6]">
+      <div className="mx-auto flex h-15.5 max-w-6xl justify-between">
+        <div className="flex h-15.5 w-20.75 items-center justify-center">
+          Apron
+        </div>
+      </div>
+    </header>
+  );
+}
