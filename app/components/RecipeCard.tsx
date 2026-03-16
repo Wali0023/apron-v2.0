5e58d886dbacae699308f23f7814fd7a1d5ff753
Ulfath <ulfathwaliullah@gmail.com>
2026-03-16 01:15:07 +0100

    Add RecipeCard

---
+++ app/components/RecipeCard.tsx
@@ -0,0 +1,22 @@
+import { Recipe } from "../types";
+
+export default function RecipeCard({ recipe }: { recipe: Recipe }) {
+  return (
+    <div className="h-64 w-64 cursor-pointer overflow-hidden rounded-xl border border-slate-200 bg-white p-4 shadow-md transition-shadow duration-300 hover:shadow-lg">
+      <div className="h-32">
+        <img
+          className="h-full w-full rounded-xl object-cover"
+          src={recipe.imageUrl}
+        />
+      </div>
+      <div className="h-32">
+        <span className="mt-1 inline-block rounded-full bg-orange-100 px-3 py-1 text-sm font-semibold text-orange-600">
+          {recipe.cuisine}
+        </span>
+        <h2 className="mb-2 text-xl font-bold text-slate-800">
+          {recipe.title}
+        </h2>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
+import RecipeCard from "./components/RecipeCard";
 import type { Recipe, RecipesResponse } from "./types";
-import Link from "next/link";
 
 export default async function Home() {
   const API_URL = "http://localhost:4000";
@@ -16,22 +16,7 @@ export default async function Home() {
       <div className="mx-auto grid max-w-6xl grid-cols-1 gap-6 p-4 sm:grid-cols-2 lg:grid-cols-4">
         {recipes.map((recipe: Recipe) => (
           <div key={recipe.id}>
-            <div className="h-64 w-64 overflow-hidden rounded-xl border border-slate-200 bg-white p-4 shadow-md transition-shadow duration-300 hover:shadow-lg">
-              <div className="h-32">
-                <img
-                  className="h-full w-full rounded-xl object-cover"
-                  src={recipe.imageUrl}
-                />
-              </div>
-              <div className="h-32">
-                <span className="mt-1 inline-block rounded-full bg-orange-100 px-3 py-1 text-sm font-semibold text-orange-600">
-                  {recipe.cuisine}
-                </span>
-                <h2 className="mb-2 text-xl font-bold text-slate-800">
-                  {recipe.title}
-                </h2>
-              </div>
-            </div>
+            <RecipeCard recipe={recipe} />
           </div>
         ))}
       </div>
