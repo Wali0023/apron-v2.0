8ae90ab37166f25b5b570a0412be8a34abecaba7
Ulfath <ulfathwaliullah@gmail.com>
2026-03-16 07:06:37 +0100

    Add styling to delete and edit buttons or recipe card

--- app/components/DeleteForm.tsx
+++ app/components/DeleteForm.tsx
@@ -1,11 +1,14 @@
 import Form from "next/form";
 import { deleteRecipe } from "../recipes/create/action";
+import { SquarePen, Trash2 } from "lucide-react";
 
 export function DeleteForm({ id }: { id: string }) {
   return (
     <Form action={deleteRecipe}>
       <input hidden readOnly name="id" value={id} />
-      <button type="submit">Delete</button>
+      <button type="submit" className="cursor-pointer">
+        <Trash2 className="w-4 h-4 text-red-600" />
+      </button>
     </Form>
   );
 }
--- app/components/RecipeCard.tsx
+++ app/components/RecipeCard.tsx
@@ -1,25 +1,39 @@
 import Link from "next/link";
 import { Recipe } from "../types";
 import { DeleteForm } from "./DeleteForm";
+import { SquarePen, Trash2 } from "lucide-react";
 
 export default function RecipeCard({ recipe }: { recipe: Recipe }) {
   return (
-    <div className="h-64 w-64 cursor-pointer overflow-hidden rounded-xl border border-slate-200 bg-white p-4 shadow-md transition-shadow duration-300 hover:shadow-lg">
+    <div className="group relative h-64 w-64 cursor-pointer overflow-hidden rounded-xl border border-slate-200 bg-white  shadow-md transition-shadow duration-300 hover:shadow-lg">
       <div className="h-32">
         <img
           className="h-full w-full rounded-xl object-cover"
           src={recipe.imageUrl}
         />
       </div>
-      <div className="h-32">
+      <div className="h-32 p-4">
         <span className="mt-1 inline-block rounded-full bg-orange-100 px-3 py-1 text-sm font-semibold text-orange-600">
           {recipe.cuisine}
         </span>
         <h2 className="mb-2 text-xl font-bold text-slate-800">
           {recipe.title}
         </h2>
-        <Link href={`/recipes/edit/${recipe.id}`}>Edit</Link>
-        <DeleteForm id={recipe.id.toString()} />
+        <div
+          className="flex gap-2 items-center opacity-0 group-hover:opacity-100 transition-opacity duration-200 absolute bottom-2 right-2"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <Link
+            href={`/recipes/edit/${recipe.id}`}
+            className="p-1 hover:bg-blue-50 rounded-full transition-colors"
+          >
+            <SquarePen className="w-5 h-5 text-blue-600" />
+          </Link>
+
+          <div className="p-1 hover:bg-red-50 rounded-full transition-colors">
+            <DeleteForm id={recipe.id.toString()} />
+          </div>
+        </div>
       </div>
     </div>
   );
